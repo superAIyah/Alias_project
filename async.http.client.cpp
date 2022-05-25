59f84d819eaff441c9f12e2010070b0ce8e8077a
Nizhametdinov Mansur <mansur.sh.n@yandex.ru>
2022-05-25 12:42:37 +0300

    Commit 2

--- async.http.client.cpp
+++ async.http.client.cpp
@@ -34,128 +34,30 @@ class Client
   }
 
  private:
-  std::string readUntil(char until, const char* arr, int &counter)
-    {
-        std::string result = "";
-        while (arr[counter] != until) {
-            result += recieved_[counter++];
-        }
+  typedef struct {
+    std::string login;
+    int difficulty;
+    int num_of_players;
+    int num_of_teams;
+    int round_duration;
+  } send_settings_struct;
 
-        return result;
+  typedef struct {
+    int game_id;
+    int team_id;
+    std::vector <std::string> team;
+  } get_settings_struct;
+
+  void Process()
+  {
+//    std::string protocol = get_from_gui();
+    if (protocol == "settings") {
+      // Получил данные от Феди
+      // char[10000] send = data – Данные в строке для Влада
     }
-
-    void static CopyFromStringToCharArray(const std::string& str, size_t str_size, char* arr)
-    {
-        for (size_t counter = 0; counter < str_size; ++counter)
-        {
-            arr[counter] = str[counter];
-        }
+    else {
     }
-
-    std::string PrepareStringTasksResponse(std::vector<Task> arr, UserDBManager& UDBM)
-    {
-        std::string result = "";
-        size_t size = arr.size();
-        if (arr.size() > 0)
-        {
-            for (size_t counter = 0; counter < size - 1; ++counter)
-            {
-                result += std::to_string(arr[counter].id) + ":" + arr[counter].head + ":" + UDBM.get_user(arr[counter].assigner_id).login
-                          + ":" + UDBM.get_user(arr[counter].executor_id).login + ":" + (arr[counter].completion ? "1":"0") + ":";
-            }
-            result += std::to_string(arr[size - 1].id) + ":" + arr[size - 1].head + ":" + UDBM.get_user(arr[size - 1].assigner_id).login
-                      + ":" + UDBM.get_user(arr[size - 1].executor_id).login + ":" + (arr[size-1].completion ? "1":"0");
-        }
-
-        return result;
-    }
-
-    void EmptyBuffer(char* arr)
-    {
-        for (size_t counter = 0; counter < max_length; ++counter)
-        {
-            arr[counter] = '\r';
-        }
-    }
-
-    void Process()
-    {
-        UserDBManager UDBM(DB_conn.session);
-        TaskDBManager TDBM(DB_conn.session);
-
-        int i = 0, task_id = 0;
-        std::string request, username, head, assigner, executor, task_state;
-        request = readUntil(':', recieved_, i);
-
-        ++i;
-
-        if (request == "authorization") {
-            username = readUntil('\r', recieved_, i);
-
-            User usr(username);
-            UDBM.add_user(usr);
-            int id = UDBM.search_user(username).id;
-            to_send = username + ":" + std::to_string(id) + "\r" + "\n";
-            CopyFromStringToCharArray(to_send, to_send.size(), sent_);
-
-            send_required = true;
-        }
-        else {
-            if (request == "add") {
-                head = readUntil(':', recieved_, i);
-                ++i;
-                assigner = readUntil(':', recieved_, i);
-                ++i;
-                executor = readUntil('\r', recieved_, i);
-                try{
-                    int assigner_id = UDBM.search_user(assigner).id;
-                    int executor_id = UDBM.search_user(executor).id;
-                    Task tsk(head, assigner_id, executor_id);
-                    TDBM.add_task(tsk);
-                }
-                catch (DB_except) {}
-
-                send_required = false;
-            }
-            else {
-                if (request == "get") {
-                    std::vector<Task> tasks;
-                    username = readUntil('\r', recieved_, i);
-                    int id = UDBM.search_user(username).id;
-                    tasks = TDBM.get_user_tasks(id);
-                    to_send = std::to_string(tasks.size()) + ":"
-                              + PrepareStringTasksResponse(tasks, UDBM)
-                              + "\r" + "\n";
-                    CopyFromStringToCharArray(to_send, to_send.size(), sent_);
-
-                    send_required = true;
-                } else {
-                    if (request == "edit") {
-                        task_id = std::stoi(readUntil(':', recieved_, i));
-                        ++i;
-                        head = readUntil(':', recieved_, i);
-                        ++i;
-                        assigner = readUntil(':', recieved_, i);
-                        ++i;
-                        executor = readUntil(':', recieved_, i);
-                        ++i;
-                        task_state = readUntil('\r', recieved_, i);
-                        try {
-                            if (task_state == "0") {
-                                TDBM.incomplete_task(task_id);
-                            } else {
-                                TDBM.complete_task(task_id);
-                            }
-                        }
-                        catch (DB_except) {}
-
-                        send_required = false;
-                    }
-                }
-            }
-        }
-        EmptyBuffer(recieved_);
-    }
+  }
   void handle_resolve(const boost::system::error_code& err,
                       const tcp::resolver::results_type& endpoints)
   {
@@ -281,6 +183,9 @@ class Client
   }
 
  private:
+  send_settings_struct settings;
+  char sent_[max_length];
+
   tcp::resolver resolver_;
   tcp::socket socket_;
 
